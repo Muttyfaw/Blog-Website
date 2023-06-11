0830cb16a23c37d1c8bdac5bc79aef13c09f2c29
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-11 20:31:39 +0000

    Day 5

--- app.js
+++ app.js
@@ -43,18 +43,18 @@ app.post("/compose", function(req, res){
 
 })
 
-// app.post("/", function(req, res){
-//   res.redirect("/")
-// })
-
-
-
-
-
-
-
-
-
+app.get("/posts/:postTitle", function (req, res) {
+  const requestedTitle = req.params.postTitle 
+ 
+  posts.forEach(function(post){
+    const storedTitle = post.title
+    if (storedTitle == requestedTitle ){
+      console.log("Match found")
+    }
+    else{console.log("Not a match")}
+    
+  })
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
